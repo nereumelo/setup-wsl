6ef323d74af163d8b05daec02011c6290b4ef438
Nereu Melo <52723155+nereumelo@users.noreply.github.com>
2023-10-08 11:19:14 -0300

    Update install-dotnet.sh

--- scripts/bash/install-dotnet.sh
+++ scripts/bash/install-dotnet.sh
@@ -4,5 +4,10 @@ sudo dpkg -i libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh &&  ./dotnet-install.sh --channel 5.0
 
-export DOTNET_ROOT=$HOME/.dotnet
-export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+# export DOTNET_ROOT=$HOME/.dotnet
+# export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+
+echo "$(curl -fsSL https://raw.githubusercontent.com/nereumelo/setup-wsl/main/props/dotnet)" >> ~/.bashrc
+source ~/.bashrc
+
+[[ -n $(dotnet -v) ]] 2> /dev/null && echo -e '\n\033[0;.NET ready to use\033[0m' || echo -e '\n\033[1;33mYou should reboot\033[0m'
